d77090babaa6ffb1e0f31fbc8e3f456ce7451c02
Austin George <ageorge8000@gmail.com>
2020-07-11 15:48:30 -0400

    Added Tilted Divider to About Section

--- src/About.js
+++ src/About.js
@@ -7,6 +7,13 @@ function About(){
       <div id="content" className="aboutContent">
       <h1 className="title">About</h1>
       <p>{placeholder}</p>
+      <p>{placeholder}</p>
+      </div>
+
+      <div className="aboutTilt">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+              <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
+          </svg>
       </div>
     </section>
   );
